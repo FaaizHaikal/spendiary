feeaca6ac9e9bc4046b89b29687cbb2feeeeab10
FaaizHaikal <faaizhaikal@gmail.com>
2025-04-24 23:45:27 +0700

    feat: app theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+import '../constants/app_fonts.dart';
+
+class AppTheme {
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.background,
+    fontFamily: AppFonts.primary,
+    colorScheme: ColorScheme.dark(
+      primary: AppColors.primary,
+      secondary: AppColors.secondary,
+    ),
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      bodySmall: TextStyle(fontSize: 12)
+    ),
+  );
+}
