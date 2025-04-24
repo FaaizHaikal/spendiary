4672a83f9263cf427de06dea92818efdf60b5e87
FaaizHaikal <faaizhaikal@gmail.com>
2025-04-24 23:45:22 +0700

    feat: app colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color.fromRGBO(20, 17, 15, 1);
+  static const primary = Color.fromRGBO(46, 46, 46, 1);
+  static const secondary = Color.fromRGBO(58, 58, 58, 1);
+
+  static const bluePrimary = Color.fromRGBO(0, 191, 255, 1);
+  static const blueSecondary = Color.fromRGBO(63, 136, 197, 1);
+
+  static const redPrimary = Color.fromRGBO(214, 48, 49, 1);
+  static const redSecondary = Color.fromRGBO(254, 95, 85, 1);
+
+  static const greenPrimary = Color.fromRGBO(21, 255, 0, 1);
+
+  static const textPrimary = Color.fromRGBO(246, 247, 235, 0.87);
+  static const textSecondary = Color.fromRGBO(246, 247, 235, 0.6);
+}
