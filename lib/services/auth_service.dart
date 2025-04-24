965e75c73daedf0d5f220e76bc685b9b64790c67
FaaizHaikal <faaizhaikal@gmail.com>
2025-04-25 01:08:12 +0700

    feat: add auth response

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:spendiary/services/storage_service.dart';
 import '../core/contants.dart';
 
+class AuthResponse {
+  final bool success;
+  final String? message;
+
+  AuthResponse({required this.success, this.message});
+}
+
 class AuthService {
-  static Future<bool> login(String username, String password) async {
+  static Future<AuthResponse> login(String username, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/api/login'),
       headers: {'Content-Type': 'application/json'},
@@ -16,17 +24,24 @@ class AuthService {
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString('access_token', data['access_token']);
-      await prefs.setString('refresh_token', data['refresh_token']);
 
-      return true;
+      await StorageService.saveTokens(
+        data['access_token'],
+        data['refresh_token'],
+      );
+
+      return AuthResponse(success: true);
     } else {
-      return false;
+      final data = jsonDecode(response.body);
+      
+      return AuthResponse(
+        success: false,
+        message: data['message'] ?? 'Login failed',
+      );
     }
   }
 
-  static Future<bool> register(String username, String password) async {
+  static Future<AuthResponse> register(String username, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/api/register'),
       headers: {'Content-Type': 'application/json'},
@@ -36,7 +51,16 @@ class AuthService {
       }),
     );
 
-    return response.statusCode == 201;
+    if (response.statusCode == 201) {
+      return AuthResponse(success: true);
+    } else {
+      final data = jsonDecode(response.body);
+      
+      return AuthResponse(
+        success: false,
+        message: data['message'] ?? 'Registration failed',
+      );
+    }
   }
 
   static Future<void> logout() async {
