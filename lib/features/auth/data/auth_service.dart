b5b77650cd3675d0033340faadda484b880c4a61
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-01 09:48:04 +0700

    feat: add verify access token service

--- lib/features/auth/data/auth_service.dart
+++ lib/features/auth/data/auth_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:spendiary/core/contants.dart';
+import 'package:spendiary/core/services/storage_service.dart';
 import 'models/auth_response.dart';
 
 class AuthService {
@@ -13,13 +14,13 @@ class AuthService {
 
     final data = jsonDecode(response.body);
 
-    if (response.statusCode == 200) {
-      return AuthResponse.fromJson(data);
-    } else {
+    if (response.statusCode != 200) {
       final error = data['error'] ?? 'Failed to login';
 
       throw Exception(error);
     }
+
+    return AuthResponse.fromJson(data);
   }
 
   static Future<void> register(String username, String password) async {
@@ -29,12 +30,48 @@ class AuthService {
       body: jsonEncode({'username': username, 'password': password}),
     );
 
-    if (response.statusCode == 201) {
-      return;
-    } else {
+    if (response.statusCode != 201) {
       final error = jsonDecode(response.body)['error'] ?? 'Failed to register';
 
       throw Exception(error);
     }
+  }
+
+  static Future<bool> verifyAccessToken() async {
+    final accessToken = await StorageService.getAccessToken();
+
+    if (accessToken == null) {
+      return false;
+    }
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/api/user/verify'),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+
+    return response.statusCode == 200;
+  }
+
+  static Future<bool> refreshAccessToken() async {
+    final refreshToken = await StorageService.getRefreshToken();
+
+    if (refreshToken == null) {
+      return false;
+    }
+
+    final response = await http.post(
+      Uri.parse('$baseUrl/api/refresh'),
+      body: jsonEncode({'refresh_token': refreshToken}),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode != 200) {
+      return false;
+    }
+
+    final accessToken = jsonDecode(response.body)['access_token'];
+    await StorageService.saveAccessToken(accessToken);
+
+    return true;
   }
 }
