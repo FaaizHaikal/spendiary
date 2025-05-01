6acbe1c7a24508b54b6f0c616b1448b05f0074f1
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-01 10:37:30 +0700

    refactor: change http to dio client

--- lib/features/auth/data/auth_service.dart
+++ lib/features/auth/data/auth_service.dart
@@ -1,53 +1,38 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:spendiary/core/contants.dart';
+import 'package:spendiary/core/services/dio_client.dart';
 import 'package:spendiary/core/services/storage_service.dart';
 import 'models/auth_response.dart';
 
 class AuthService {
   static Future<AuthResponse> login(String username, String password) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/api/login'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'username': username, 'password': password}),
+    final response = await DioClient.dio.post(
+      '/api/login',
+      data: {'username': username, 'password': password},
     );
 
-    final data = jsonDecode(response.body);
-
     if (response.statusCode != 200) {
-      final error = data['error'] ?? 'Failed to login';
+      final error = response.data['error'] ?? 'Failed to login';
 
       throw Exception(error);
     }
 
-    return AuthResponse.fromJson(data);
+    return AuthResponse.fromJson(response.data);
   }
 
   static Future<void> register(String username, String password) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/api/register'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'username': username, 'password': password}),
+    final response = await DioClient.dio.post(
+      '/api/register',
+      data: {'username': username, 'password': password},
     );
 
     if (response.statusCode != 201) {
-      final error = jsonDecode(response.body)['error'] ?? 'Failed to register';
+      final error = response.data['error'] ?? 'Failed to register';
 
       throw Exception(error);
     }
   }
 
   static Future<bool> verifyAccessToken() async {
-    final accessToken = await StorageService.getAccessToken();
-
-    if (accessToken == null) {
-      return false;
-    }
-
-    final response = await http.get(
-      Uri.parse('$baseUrl/api/user/verify'),
-      headers: {'Authorization': 'Bearer $accessToken'},
-    );
+    final response = await DioClient.dio.get('/api/user/verify');
 
     return response.statusCode == 200;
   }
@@ -59,17 +44,16 @@ class AuthService {
       return false;
     }
 
-    final response = await http.post(
-      Uri.parse('$baseUrl/api/refresh'),
-      body: jsonEncode({'refresh_token': refreshToken}),
-      headers: {'Content-Type': 'application/json'},
+    final response = await DioClient.dio.post(
+      '/api/refresh',
+      data: {'refresh_token': refreshToken},
     );
 
     if (response.statusCode != 200) {
       return false;
     }
 
-    final accessToken = jsonDecode(response.body)['access_token'];
+    final accessToken = response.data['access_token'];
     await StorageService.saveAccessToken(accessToken);
 
     return true;
--- lib/features/dashboard/data/expenses_service.dart
+++ lib/features/dashboard/data/expenses_service.dart
@@ -1,71 +1,40 @@
 import 'dart:convert';
-import 'package:spendiary/core/contants.dart';
-import 'package:spendiary/core/services/storage_service.dart';
+import 'package:spendiary/core/services/dio_client.dart';
 import 'package:spendiary/core/models/expense.dart';
-import 'package:http/http.dart' as http;
 import 'package:spendiary/features/dashboard/data/models/chart_point.dart';
 
 class ExpenseService {
   static Future<List<ChartPoint>> getExpensesByPeriod(String period) async {
-    final accessToken = await StorageService.getAccessToken();
-
-    if (accessToken == null) {
-      throw Exception('Missing access token');
-    }
-
-    final uri = Uri.parse(
-      '$baseUrl/api/user/expenses/group',
-    ).replace(queryParameters: {'period': period});
-
-    final response = await http.get(
-      uri,
-      headers: {
-        'Authorization': 'Bearer $accessToken',
-        'Content-Type': 'application/json',
-      },
+    final response = await DioClient.dio.get(
+      '/api/user/expenses/group',
+      queryParameters: {'period': period},
     );
 
     if (response.statusCode != 200) {
       final error =
-          json.decode(response.body)['error'] ?? 'Failed to fetch expenses';
+          json.decode(response.data)['error'] ?? 'Failed to fetch expenses';
       throw Exception(error);
     }
 
-    final List<dynamic> jsonList = jsonDecode(response.body);
-
-    return jsonList.map((e) => ChartPoint.fromJson(e)).toList();
+    return (response.data as List)
+        .map((item) => ChartPoint.fromJson(item))
+        .toList();
   }
 
   static Future<List<Expense>> getCurrentMonthExpenses() async {
-    final accessToken = await StorageService.getAccessToken();
-
-    if (accessToken == null) {
-      throw Exception('Missing access token');
-    }
-
+    final month = DateTime.now().year.toString();
     final year = DateTime.now().year.toString();
-    final month = DateTime.now().month.toString();
 
-    final uri = Uri.parse(
-      '$baseUrl/api/user/expenses/monthly',
-    ).replace(queryParameters: {'year': year, 'month': month});
-
-    final response = await http.get(
-      uri,
-      headers: {
-        'Authorization': 'Bearer $accessToken',
-        'Content-Type': 'application/json',
-      },
+    final response = await DioClient.dio.get(
+      '/api/user/expenses/monthly',
+      queryParameters: {'year': year, 'month': month},
     );
 
     if (response.statusCode != 200) {
-      final error =
-          json.decode(response.body)['error'] ?? 'Failed to fetch expenses';
+      final error = response.data['error'] ?? 'Failed to fetch expenses';
       throw Exception(error);
     }
 
-    final List<dynamic> jsonList = json.decode(response.body);
-
-    return jsonList.map((json) => Expense.fromJson(json)).toList();
+    return response.data.map((item) => Expense.fromJson(item)).toList();
   }
 }
