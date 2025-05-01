0417bafc0adf283ee62d2935d561f5765798388a
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-01 09:53:20 +0700

    feat: expenses logic

---
+++ lib/features/dashboard/logic/expenses_controller.dart
@@ -0,0 +1,21 @@
+import 'package:spendiary/core/models/expense.dart';
+import 'package:spendiary/features/dashboard/data/expenses_service.dart';
+import 'package:spendiary/features/dashboard/data/models/chart_point.dart';
+
+class ExpensesController {
+  static Future<List<ChartPoint>> getExpensesByPeriod(String period) async {
+    try {
+      return await ExpenseService.getExpensesByPeriod(period);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  static Future<List<Expense>> getCurrentMonthExpenses() async {
+    try {
+      return await ExpenseService.getCurrentMonthExpenses();
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
