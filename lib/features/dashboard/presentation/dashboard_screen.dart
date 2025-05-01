337596099758b2d17e4e0aa9fbcb2fb839d48e9a
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-01 09:52:02 +0700

    feat: progress on dashboard screen

--- lib/features/dashboard/presentation/dashboard_screen.dart
+++ lib/features/dashboard/presentation/dashboard_screen.dart
@@ -1,1 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:spendiary/features/dashboard/presentation/widgets/expenses_content.dart';
+import 'package:spendiary/features/dashboard/presentation/widgets/toggle_content.dart';
 
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(24.0),
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            const Text('Total Expenses', style: TextStyle(fontSize: 16)),
+            const SizedBox(height: 4),
+            const Text(
+              'Rp. 1.000.000',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            ToggleContent(
+              options: const ['Expenses', 'Savings'],
+              selectedIndex: _selectedIndex,
+              onSelected: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            if (_selectedIndex == 0) ...[
+              ExpensesContent(),
+            ] else ...[
+              const Text('Savings'),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
