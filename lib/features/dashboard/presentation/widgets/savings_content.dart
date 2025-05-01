ec8b3feeac969f8d4d41d64d76d15769c12f8157
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-01 09:55:19 +0700

    chore: add unimplemented savings content

---
+++ lib/features/dashboard/presentation/widgets/savings_content.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SavingsContent extends StatefulWidget {
+  const SavingsContent({super.key});
+
+  @override
+  State<SavingsContent> createState() => _SavingsContentState();
+}
+
+class _SavingsContentState extends State<SavingsContent> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
