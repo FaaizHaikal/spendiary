5759449d55c8490e4871fb12ee8b2f43e271c369
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-13 21:40:33 +0700

    feat: expenses state

---
+++ lib/features/dashboard/logic/expenses_state.dart
@@ -0,0 +1,51 @@
+import 'package:spendiary/core/models/expense.dart';
+import 'package:spendiary/features/dashboard/data/models/chart_point.dart';
+
+class ExpensesState {
+  final Map<String, List<ChartPoint>> allChartData;
+  final List<Expense> recentExpenses;
+  final int selectedPeriodIndex;
+  final bool isChartLoading;
+  final bool isRecentLoading;
+
+  static const periods = ['Day', 'Week', 'Month', 'Year'];
+
+  ExpensesState({
+    required this.allChartData,
+    required this.recentExpenses,
+    required this.selectedPeriodIndex,
+    required this.isChartLoading,
+    required this.isRecentLoading,
+  });
+
+  factory ExpensesState.initial() => ExpensesState(
+    allChartData: {},
+    recentExpenses: [],
+    selectedPeriodIndex: 2,
+    isChartLoading: false,
+    isRecentLoading: false,
+  );
+
+  String get currentPeriod => periods[selectedPeriodIndex];
+
+  List<ChartPoint> get currentChartData => allChartData[currentPeriod] ?? [];
+
+  double get total =>
+      currentChartData.fold(0.0, (sum, item) => sum + item.total);
+
+  ExpensesState copyWith({
+    Map<String, List<ChartPoint>>? allChartData,
+    List<Expense>? recentExpenses,
+    int? selectedPeriodIndex,
+    bool? isChartLoading,
+    bool? isRecentLoading,
+  }) {
+    return ExpensesState(
+      allChartData: allChartData ?? this.allChartData,
+      recentExpenses: recentExpenses ?? this.recentExpenses,
+      selectedPeriodIndex: selectedPeriodIndex ?? this.selectedPeriodIndex,
+      isChartLoading: isChartLoading ?? this.isChartLoading,
+      isRecentLoading: isRecentLoading ?? this.isRecentLoading,
+    );
+  }
+}
