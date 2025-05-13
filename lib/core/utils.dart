2334bb4dfd06b72ffc7cfbd1e99644be938ec2d8
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-13 22:34:02 +0700

    fix: fix currency formatter

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -4,27 +4,34 @@ class AppUtils {
   static NumberFormat _idrFormatter({
     int decimalDigits = 2,
     String symbol = 'Rp. ',
+    bool compact = false,
   }) {
-    return NumberFormat.currency(
-      locale: 'id_ID',
-      symbol: symbol,
-      decimalDigits: decimalDigits,
-    );
+    return compact
+        ? NumberFormat.compactCurrency(
+            locale: 'id_ID',
+            symbol: symbol,
+            decimalDigits: decimalDigits,
+          )
+        : NumberFormat.currency(
+            locale: 'id_ID',
+            symbol: symbol,
+            decimalDigits: decimalDigits,
+          );
   }
 }
 
 extension CurrencyFormatting on num? {
-String toIDR({
+  String toIDR({
     int decimalDigits = 2,
     String symbol = 'Rp. ',
     bool compact = false,
     String fallback = '-',
   }) {
     if (this == null) return fallback;
-    return this!.toIDR(
+    return AppUtils._idrFormatter(
       decimalDigits: decimalDigits,
       symbol: symbol,
       compact: compact,
-    );
+    ).format(this!); // Format directly using the formatter
   }
 }
