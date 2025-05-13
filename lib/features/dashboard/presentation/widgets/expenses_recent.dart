27c96d1e57fb637d2ebccd39e4f7b6d50692c126
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-13 19:59:00 +0700

    chore: prettier

--- lib/features/dashboard/presentation/widgets/expenses_recent.dart
+++ lib/features/dashboard/presentation/widgets/expenses_recent.dart
@@ -19,15 +19,11 @@ class ExpensesRecent extends StatelessWidget {
             children: [
               const Text(
                 'Recent',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               TextButton(
                 onPressed: () {
                   // Navigator.push(context, MaterialPageRoute(builder: (_) => const ExpenseScreen()));
-
                 },
                 style: TextButton.styleFrom(
                   padding: EdgeInsets.zero,
@@ -36,105 +32,110 @@ class ExpensesRecent extends StatelessWidget {
                 ),
                 child: const Text(
                   'See All >',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: AppColors.textPrimary
-                  ),
+                  style: TextStyle(fontSize: 12, color: AppColors.textPrimary),
                 ),
               ),
             ],
           ),
         ),
-ConstrainedBox(
+        ConstrainedBox(
           constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(context).size.height * 0.25, // Adjust as needed
+            maxHeight:
+                MediaQuery.of(context).size.height * 0.25, // Adjust as needed
           ),
           child: SingleChildScrollView(
             scrollDirection: Axis.vertical,
-            child: Column(children: data.asMap().entries.map((entry) {
-                final index = entry.key;
-                final expense = entry.value;
-                final isOdd = index.isOdd;
-            
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Date/Time and Description
-                      Row(
+            child: Column(
+              children:
+                  data.asMap().entries.map((entry) {
+                    final index = entry.key;
+                    final expense = entry.value;
+                    final isOdd = index.isOdd;
+
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          // Date Circle
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color:
-                                  isOdd
-                                      ? AppColors.primaryAccent
-                                      : AppColors.secondary,
-                            ),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
+                          // Date/Time and Description
+                          Row(
+                            children: [
+                              // Date Circle
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color:
+                                      isOdd
+                                          ? AppColors.primaryAccent
+                                          : AppColors.secondary,
+                                ),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12,
+                                          height: 1.1,
+                                        ),
+                                        expense.date.day.toString(),
+                                      ),
+                                      Text(
+                                        _getMonthAbbreviation(
+                                          expense.date.month,
+                                        ),
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          height: 1.1,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+
+                              // Description and Time
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
-                                    style: TextStyle(
+                                    expense.description,
+                                    style: const TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 12,
-                                      height: 1.1
+                                      height: 0.9,
                                     ),
-                                    expense.date.day.toString(),
                                   ),
+                                  const SizedBox(height: 4),
                                   Text(
-                                    _getMonthAbbreviation(expense.date.month),
-                                    style: TextStyle(fontSize: 12, height: 1.1),
+                                    style: TextStyle(fontSize: 12, height: 0.9),
+                                    '${expense.date.hour.toString().padLeft(2, '0')}:${expense.date.minute.toString().padLeft(2, '0')}',
                                   ),
                                 ],
                               ),
-                            ),
-                          ),
-                          const SizedBox(width: 12),
-            
-                          // Description and Time
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                expense.description,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12,
-                                  height: 0.9
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                style: TextStyle(fontSize: 12, height: 0.9),
-                                '${expense.date.hour.toString().padLeft(2, '0')}:${expense.date.minute.toString().padLeft(2, '0')}',
-                              ),
                             ],
+                          ),
+
+                          // Amount
+                          Text(
+                            '-${_formatCurrency(expense.amount)}',
+                            style: const TextStyle(
+                              color: AppColors.redAccent,
+                              fontSize: 12,
+                            ),
                           ),
                         ],
                       ),
-            
-                      // Amount
-                      Text(
-                        '-${_formatCurrency(expense.amount)}',
-                        style: const TextStyle(
-                          color: AppColors.redAccent,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }).toList()
+                    );
+                  }).toList(),
             ),
           ),
         ),
-      ]
+      ],
     );
   }
 
