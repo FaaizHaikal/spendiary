ea915dfeb44cce5f6c76bbc3e40a40f6ec176368
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-14 19:22:06 +0700

    feat: add description attribute

--- lib/core/models/saving.dart
+++ lib/core/models/saving.dart
@@ -1,12 +1,14 @@
 class Saving {
   final DateTime date;
+  final String description;
   final double amount;
 
-  Saving({required this.date, required this.amount});
+  Saving({required this.date, required this.description, required this.amount});
 
   factory Saving.fromJson(Map<String, dynamic> json) {
     return Saving(
       date: DateTime.parse(json['Date']),
+      description: json['Description'],
       amount: (json['Amount'] as num).toDouble(),
     );
   }
